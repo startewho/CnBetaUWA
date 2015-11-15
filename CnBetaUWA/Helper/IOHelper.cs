6d93e220bdeb881465aef8c6b765d0cd8f626c3d
HelyCheng <hoolooday@gmail.com>
2015-11-15 21:07:21 +0800

    1115

--- CnBetaUWA/Helper/IOHelper.cs
+++ CnBetaUWA/Helper/IOHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.Search;
 using Q42.WinRT;
 using UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding;
 
@@ -38,6 +40,25 @@ namespace CnBetaUWA.Helper
             return result;
         }
 
+        public static async Task<double> GetFolderSize(StorageFolder folder)
+        {
+            
+            // Query all files in the folder. Make sure to add the CommonFileQuery
+            // So that it goes through all sub-folders as well
+            var folders = folder.CreateFileQuery(CommonFileQuery.OrderByName);
+
+            // Await the query, then for each file create a new Task which gets the size
+            var fileSizeTasks = (await folders.GetFilesAsync()).Select(async file => (await file.GetBasicPropertiesAsync()).Size);
+
+            // Wait for all of these tasks to complete. WhenAll thankfully returns each result
+            // as a whole list
+            var sizes = await Task.WhenAll(fileSizeTasks);
+
+            // Sum all of them up. You have to convert it to a long because Sum does not accept ulong.
+            var folderSize = sizes.Sum(l => (long)l);
+
+            return folderSize;
+        }
 
     }
 }
---
+++ CnBetaUWA/ViewModels/SettingCachePage_Model.cs
@@ -0,0 +1,56 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Windows.Storage;
+using CnBetaUWA.Helper;
+
+namespace CnBetaUWA.ViewModels
+{
+
+    [DataContract]
+    public class SettingCachePage_Model : ViewModelBase<SettingCachePage_Model>
+    {
+        // If you have install the code sniplets, use "propvm + [tab] +[tab]" create a property。
+        // 如果您已经安装了 MVVMSidekick 代码片段，请用 propvm +tab +tab 输入属性
+        public SettingCachePage_Model()
+        {
+            InitData();
+        }
+
+
+        private async void InitData()
+        {
+            var cachefolder = ApplicationData.Current.LocalFolder;
+            var size = await IOHelper.GetFolderSize(cachefolder);
+            CacheSize = size/1024/1024;
+
+        }
+
+
+        public double CacheSize
+        {
+            get { return _CacheSizeLocator(this).Value; }
+            set { _CacheSizeLocator(this).SetValueAndTryNotify(value); }
+        }
+        #region Property double CacheSize Setup        
+        protected Property<double> _CacheSize = new Property<double> { LocatorFunc = _CacheSizeLocator };
+        static Func<BindableBase, ValueContainer<double>> _CacheSizeLocator = RegisterContainerLocator<double>("CacheSize", model => model.Initialize("CacheSize", ref model._CacheSize, ref _CacheSizeLocator, _CacheSizeDefaultValueFactory));
+        static Func<double> _CacheSizeDefaultValueFactory = () => default(double);
+        #endregion
+
+
+    }
+
+}
+
